28c29da10a31a657a6a9b1916b1dbf616c5544f8
Shane <innon.shen@gmail.com>
2022-02-17 17:06:29 +0800

    [new] listBuckets + getBucket

---
+++ src/main/java/com/linklogis/S3.java
@@ -0,0 +1,39 @@
+package com.linklogis;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+
+import java.util.List;
+
+public class S3 {
+
+    final static AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
+
+    /**
+     * 列出所有的桶
+     *
+     * @return 返回所有的桶
+     */
+    public static List<Bucket> listBucket() {
+        return s3.listBuckets();
+    }
+
+    /**
+     * 列出指定的桶
+     *
+     * @param bucketName bucket 的名称
+     * @return 返回指定名称的桶，没有的话返回 null
+     */
+    public static Bucket getBucket(String bucketName) {
+        Bucket targetBucket = null;
+        List<Bucket> buckets = s3.listBuckets();
+        for (Bucket b : buckets) {
+            if (b.getName().equals(bucketName)) {
+                targetBucket = b;
+            }
+        }
+        return targetBucket;
+    }
+
+}
