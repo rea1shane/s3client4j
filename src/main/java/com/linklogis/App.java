751ee62862711afd3de1290267c028bf92ea9b4b
Shane <innon.shen@gmail.com>
2022-02-17 17:12:48 +0800

    [new] createBucket

--- src/main/java/com/linklogis/App.java
+++ src/main/java/com/linklogis/App.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
+        S3.createBucket("create-by-java-sdk");
         List<Bucket> buckets = S3.listBuckets();
         System.out.println("Your Amazon S3 buckets are:");
         for (Bucket b : buckets) {
--- src/main/java/com/linklogis/S3.java
+++ src/main/java/com/linklogis/S3.java
@@ -2,6 +2,7 @@ package com.linklogis;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.Bucket;
 
 import java.util.List;
@@ -22,18 +23,37 @@ public class S3 {
     /**
      * 列出指定的桶
      *
-     * @param bucketName bucket 的名称
+     * @param bucketName 桶的名称
      * @return 返回指定名称的桶，没有的话返回 null
      */
     public static Bucket getBucket(String bucketName) {
         Bucket targetBucket = null;
-        List<Bucket> buckets = s3.listBuckets();
+        List<Bucket> buckets = listBuckets();
         for (Bucket b : buckets) {
             if (b.getName().equals(bucketName)) {
                 targetBucket = b;
             }
         }
         return targetBucket;
+    }
+
+    /**
+     * @param bucketName 桶的名称
+     * @return 返回创建的指定名称的桶，如果桶已存在则会返回该桶并提示已存在
+     */
+    public static Bucket createBucket(String bucketName) {
+        Bucket b = null;
+        if (s3.doesBucketExistV2(bucketName)) {
+            System.out.format("Bucket %s already exists.\n", bucketName);
+            b = getBucket(bucketName);
+        } else {
+            try {
+                b = s3.createBucket(bucketName);
+            } catch (AmazonS3Exception e) {
+                System.err.println(e.getErrorMessage());
+            }
+        }
+        return b;
     }
 
 }
